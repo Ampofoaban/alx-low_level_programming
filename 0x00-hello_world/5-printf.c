de0c4d52d156a863b11bd357b5247c012089bc29
Ampofoaban <banampofoa10@gmail.com>
2023-02-16 08:10:20 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - printf function program
  * Return: 0 (victory)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
-}	
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
