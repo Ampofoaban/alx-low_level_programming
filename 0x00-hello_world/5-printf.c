3ba85042870fdcd198383345ded132e13028c7f8
Ampofoaban <banampofoa10@gmail.com>
2023-02-16 07:28:19 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printf function program
+ * Return: 0 (victory)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}	
