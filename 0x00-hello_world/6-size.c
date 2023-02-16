b25ba621e15f51bc1f5fdee0ad333c9726625fef
Ampofoaban <banampofoa10@gmail.com>
2023-02-16 08:50:07 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ int main(void)
 	long int i;
 	long long int a;
 	float f;
+
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i));
