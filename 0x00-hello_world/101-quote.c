1d51520bed8fe77e9f053b679cb2f82574dceeed
Ampofoaban <banampofoa10@gmail.com>
2023-02-16 09:20:19 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - unix and c progra that prints a specified output
+ * Return: 1 (victory)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
