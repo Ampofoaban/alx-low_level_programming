529931cc7ee13965459b4b3d7a75207cb2dc22dd
Ampofoaban <banampofoa10@gmail.com>
2023-02-16 07:14:20 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - puts function program
-* Return - 0 (victory)
-*/
+ * main  - puts function program
+ * Return: 0 (victory)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");	
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
