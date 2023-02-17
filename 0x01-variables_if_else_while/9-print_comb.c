629c34df36b84b966d4d48a1904e9a2c440b1235
Ampofoaban <banampofoa10@gmail.com>
2023-02-17 14:01:50 -0800

    task 8

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: o (victory)
+ *
+ */
+
+int main(void)
+
+{
+
+	int c;
+
+	for (c = '0'; c <= 9; c++)
+	{
+	putchar(c);
+	if (c != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
