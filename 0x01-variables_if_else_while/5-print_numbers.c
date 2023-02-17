6a810f586a7add05e8b26d9ce2d7dcdd893bca5e
Ampofoaban <banampofoa10@gmail.com>
2023-02-17 09:36:27 -0800

    base 10 program

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -  prints all single digit numbers of base 10 starting from 0
+ * Return: base10numbers (victory)
+ *
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
