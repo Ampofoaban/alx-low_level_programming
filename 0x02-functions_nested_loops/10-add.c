1c1fec83d75342f343eb7ef996c6cc93ba583c7e
Ampofoaban <banampofoa10@gmail.com>
2023-02-21 11:45:23 -0800

    add integer program

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - will add to integers
+ * @a: first number
+ * @b: second number
+ * Return:the addition of both integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
