353bb83d2bd7bb1e16d39a7e1a4fbe1a7884ad09
Ampofoaban <banampofoa10@gmail.com>
2023-02-21 07:55:21 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value
+ * @d: the integer to be used
+ * Return: 0 or absolute value
+ */
+
+int _abs(int d)
+{
+	if (d >= 0)
+	{
+		return (d);
+	}
+	else
+		return (-d);
+}
