a6068c43777160b754d34653830e82bdd2f2f4d8
Ampofoaban <banampofoa10@gmail.com>
2023-02-21 03:16:24 -0800

    1-alphabet program

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabets in lowercase program
+ * Return: void (victory)
+ */
+
+void print_alphabet(void)
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
