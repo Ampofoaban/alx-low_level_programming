5d63fa660abdb4bc144ce81cdd5e9e8c42ae66ac
Ampofoaban <banampofoa10@gmail.com>
2023-02-23 03:19:38 -0800

    0 to 9 followed by a newline

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - will print numbers from 0 to 9 on a newline
+ * Return: numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int d;
+
+	for (d = 0; d <= 9; d++)
+	{
+		_putchar(d + '0');
+	}
+	_putchar('\n');
+}
