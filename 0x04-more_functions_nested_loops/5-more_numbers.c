7d37406b8255d58a7317614b91d524b939102999
Ampofoaban <banampofoa10@gmail.com>
2023-02-23 08:29:18 -0800

    task 4

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,19 @@
 
 void more_numbers(void)
 {
-	int a, b;
+	int x, y;
 
-	for (a = 1; a <= 10; a++)
+	for (x = 0; x < 10; x++)
 	{
-	for (b = 0; b < 15; b++)
+	for (y = 0; y <=; y++)
 	{
-	if (b >= 10)
-		_putchar('1');
-		_putchar((b % 10) + '0');
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
 	}
 }
+
