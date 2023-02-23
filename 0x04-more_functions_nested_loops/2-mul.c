fcca7217e2467b96a84e28bb47a731468d793637
Ampofoaban <banampofoa10@gmail.com>
2023-02-23 02:39:44 -0800

    multiplier program

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - mulitply two integers
+ * @a: first intger to be multiplied
+ * @b: second integer to be multiplied
+ * Return: interger (victory)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
