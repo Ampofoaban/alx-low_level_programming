c1b697a9d03c59399210876e4b82979259cdbc9f
Ampofoaban <banampofoa10@gmail.com>
2023-02-24 01:23:10 -0800

    prime no. program

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <math.h>
 
 /**
  * main - print the largesy prime number of a certain no.
@@ -8,9 +9,9 @@
 
 int main(void)
 {
-	long number = 612852475143
 	long a, maxf;
-	double square = square(number);
+	long number = 612852475143;
+	double square = sqrt(number);
 
 	for (a = 1; a <= square; a++)
 	{
