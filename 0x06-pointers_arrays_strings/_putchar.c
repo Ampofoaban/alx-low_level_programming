8192aa61eaa147461df17aecbf21e2f15429b2c6
Ampofoaban <banampofoa10@gmail.com>
2023-03-01 03:11:14 -0800

    strcat program

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write the character d to stdout
+ * @d - the character to print
+ * Return: on success 1.
+ * on error -1 is returned, and set error appropriately
+ */
+
+int _putchar(char d)
+{
+        return (write(1, &d, 1));
+}
